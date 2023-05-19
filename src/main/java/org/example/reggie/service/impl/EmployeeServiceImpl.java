10e9da8f3fd08b546681be0935851f9b8e26bf66
gongshuiwen <shuiwen.gong@outlook.com>
2023-05-19 10:07:50 +0800

    opt: 将 UserDetailsService 的实现整合至 EmployeeServiceImpl 中

--- src/main/java/org/example/reggie/service/impl/EmployeeServiceImpl.java
+++ src/main/java/org/example/reggie/service/impl/EmployeeServiceImpl.java
@@ -1,11 +1,21 @@
 package org.example.reggie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.example.reggie.service.EmployeeService;
 import org.example.reggie.entity.Employee;
 import org.example.reggie.mapper.EmployeeMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
+public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService, UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        LambdaQueryWrapper<Employee> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Employee::getUsername, username);
+        return getOne(wrapper);
+    }
 }
