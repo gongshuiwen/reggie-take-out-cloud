171292db3e9dea0ecd5cc24a94c22feabf2a64cc
gongshuiwen <shuiwen.gong@outlook.com>
2023-08-15 11:20:40 +0800

    opt: 优化存储服务接口以及本地文件存储实现

--- src/main/java/org/example/reggie/storage/FileStorageService.java
+++ src/main/java/org/example/reggie/storage/FileStorageService.java
@@ -4,99 +4,64 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.*;
-import java.nio.file.Path;
+import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
-import java.util.stream.Stream;
 
 @Slf4j
 @Service
 public class FileStorageService implements StorageService, InitializingBean {
 
-    private static final String DEFAULT_BASE_PATH = "/data/";
+    private static final String DEFAULT_BASE_PATH = "./data/";
 
-    private boolean isClassPathResource = true;
-    private String basePath;
-
-    @Override
-    public void init() {
-        log.info("Files Stored in: {}", isClassPathResource ? "class_path:" + basePath : basePath);
-    }
+    private File basePath;
 
     @Autowired
     public void setBasePath(@Value("${reggie.base-path:}") String path) {
-        if (path.isEmpty()) {
-            this.basePath = DEFAULT_BASE_PATH;
-        } else if (path.toLowerCase().startsWith("class_path:")) {
-            this.basePath = path.substring(11);
+        if (path == null || path.isEmpty()) {
+            this.basePath = new File(DEFAULT_BASE_PATH);
         } else {
-            this.basePath = path;
-            this.isClassPathResource = false;
+            this.basePath = new File(path);
         }
     }
 
     @Override
-    public String store(MultipartFile file) throws IOException {
-        // 获取文件名后缀
-        String suffix = "";
-        String originalFilename = file.getOriginalFilename();
-        if (originalFilename != null) {
-//            String[] temp = fileName.split("\\.");
-//            if (temp.length > 1) {
-//                suffix = "." + temp[temp.length - 1];
-//            }
-            int index = originalFilename.lastIndexOf(".");
-            if (index >= 0) {
-                suffix = originalFilename.substring(index);
-            }
-        }
+    public void init() throws IOException {
+        log.info("Files Stored in: {}", basePath.getCanonicalPath());
+    }
 
-        // 使用 UUID 重新生成文件名，防止用户上传重名的文件时文件被覆盖
-        String fileName = UUID.randomUUID() + suffix;
-//        File f = new File(basePath + fileName);
-//        boolean res = f.createNewFile();
-//        int length;
-//        byte[] buff = new byte[1024 * 1024 * 2];
-//        try (InputStream is = file.getInputStream();
-//             OutputStream os = Files.newOutputStream(f.toPath());
-//        ) {
-//            while ((length = is.read(buff)) != -1) {
-//                os.write(buff, 0 , length);
-//            }
-//        }
-        File f = new File(basePath + fileName);
-        file.transferTo(f.getAbsoluteFile());
-        return fileName;
+    @Override
+    public String store(MultipartFile multipartFile) throws IOException {
+        // 使用 UUID + 文件类型后缀重新生成文件名，防止用户上传重名的文件时文件被覆盖
+        String filename = UUID.randomUUID() + getFilenameSuffix(multipartFile.getOriginalFilename());
+        File file = new File(basePath, filename);
+        multipartFile.transferTo(file.getAbsoluteFile());
+        return filename;
     }
 
     @Override
     public Resource loadAsResource(String filename){
-        return isClassPathResource ? new ClassPathResource(basePath + filename) : new FileSystemResource(basePath + filename);
+        return new FileSystemResource(new File(basePath, filename));
     }
 
     @Override
-    public Stream<Path> loadAll() {
-        return null;
-    }
-
-    @Override
-    public Path load(String filename) {
-        return null;
-    }
-
-    @Override
-    public void deleteAll() {
-
-    }
-
-    @Override
-    public void afterPropertiesSet() {
+    public void afterPropertiesSet() throws IOException {
         init();
+    }
+
+    private String getFilenameSuffix(String filename) {
+        String suffix = "";
+        if (filename != null) {
+            int index = filename.lastIndexOf(".");
+            if (index >= 0) {
+                suffix = filename.substring(index);
+            }
+        }
+        return suffix;
     }
 }
--- src/main/java/org/example/reggie/storage/StorageService.java
+++ src/main/java/org/example/reggie/storage/StorageService.java
@@ -4,20 +4,12 @@ import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.stream.Stream;
 
 public interface StorageService {
 
-    void init();
+    void init() throws IOException;
 
-    String store(MultipartFile file) throws IOException;
+    String store(MultipartFile multipartFile) throws IOException;
 
     Resource loadAsResource(String filename) throws IOException;
-
-    Stream<Path> loadAll();
-
-    Path load(String filename);
-
-    void deleteAll();
 }
