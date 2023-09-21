9a21ca9cd843a5dfdab43286ad6d29b26b0a44c5
gongshuiwen <shuiwen.gong@outlook.com>
2023-09-21 10:09:30 +0800

    doc: 更新地址簿接口文档

--- src/main/java/org/example/reggie/controller/AddressBookController.java
+++ src/main/java/org/example/reggie/controller/AddressBookController.java
@@ -1,5 +1,7 @@
 package org.example.reggie.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.example.reggie.common.BaseContext;
 import org.example.reggie.common.R;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "地址簿接口")
 @Slf4j
 @RestController
 @RequestMapping("/addressBook")
@@ -18,36 +21,43 @@ public class AddressBookController {
     @Autowired
     private AddressBookService addressBookService;
 
+    @Operation(summary = "查询单个地址簿信息")
     @GetMapping("/{id}")
     public R<AddressBook> get(@PathVariable Long id) {
         return R.success(addressBookService.getById(id));
     }
 
+    @Operation(summary = "创建地址簿信息")
     @PostMapping
     public R<AddressBook> create(@RequestBody AddressBook addressBook) {
         return R.success(addressBookService.saveWithUserId(BaseContext.getCurrentUserId(), addressBook));
     }
 
+    @Operation(summary = "更新地址簿信息")
     @PutMapping
     public R<Boolean> update(@RequestBody AddressBook addressBook) {
         return R.success(addressBookService.updateById(addressBook));
     }
 
+    @Operation(summary = "删除地址簿信息")
     @DeleteMapping
     public R<Boolean> delete(@RequestParam List<Long> ids){
         return R.success(addressBookService.removeByIds(ids));
     }
 
+    @Operation(summary = "列表查询地址簿信息")
     @GetMapping("/list")
     public R<List<AddressBook>> list() {
         return R.success(addressBookService.listByUserId(BaseContext.getCurrentUserId()));
     }
 
+    @Operation(summary = "查询用户默认地址簿")
     @GetMapping("/default")
     public R<AddressBook> getDefault() {
         return R.success(addressBookService.getUserDefaultAddressBook(BaseContext.getCurrentUserId()));
     }
 
+    @Operation(summary = "更新用户默认地址簿")
     @PutMapping("/default")
     public R<Boolean> setDefault(@RequestBody AddressBook addressBook) {
         return R.success(addressBookService.setUserDefaultAddressBook(BaseContext.getCurrentUserId(), addressBook.getId()));
