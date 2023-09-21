a1fa270e7e844cffe6ec085d675f8f333454d8f3
gongshuiwen <shuiwen.gong@outlook.com>
2023-09-21 10:06:06 +0800

    doc: 更新通用接口文档

--- src/main/java/org/example/reggie/controller/CommonController.java
+++ src/main/java/org/example/reggie/controller/CommonController.java
@@ -1,5 +1,7 @@
 package org.example.reggie.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.reggie.common.R;
 import org.example.reggie.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "通用接口")
 @RestController
 @RequestMapping("/common")
 public class CommonController {
@@ -14,11 +17,13 @@ public class CommonController {
     @Autowired
     private StorageService storageService;
 
+    @Operation(summary = "下载文件")
     @GetMapping("/download")
     public Resource download(@RequestParam String name) throws Exception {
         return storageService.loadAsResource(name);
     }
 
+    @Operation(summary = "上传文件")
     @PostMapping("/upload")
     public R<String> upload(@RequestBody MultipartFile file) throws Exception {
         return R.success(storageService.store(file));
