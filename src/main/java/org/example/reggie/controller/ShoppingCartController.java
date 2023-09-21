200936ad937aa32efdcf96d33471911084acc8a1
gongshuiwen <shuiwen.gong@outlook.com>
2023-09-21 10:28:34 +0800

    doc: 更新购物车接口文档

--- src/main/java/org/example/reggie/controller/ShoppingCartController.java
+++ src/main/java/org/example/reggie/controller/ShoppingCartController.java
@@ -1,5 +1,7 @@
 package org.example.reggie.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.example.reggie.common.BaseContext;
 import org.example.reggie.common.R;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "购物车接口")
 @Slf4j
 @RestController
 @RequestMapping("/shoppingCart")
@@ -18,21 +21,25 @@ public class ShoppingCartController {
     @Autowired
     private ShoppingCartService shoppingCartService;
 
+    @Operation(summary = "列表查询用户购物车项目")
     @GetMapping("/list")
     public R<List<ShoppingCart>> list() {
         return R.success(shoppingCartService.listUserShoppingCart(BaseContext.getCurrentUserId()));
     }
 
+    @Operation(summary = "增加购物车项目")
     @PostMapping("/add")
     public R<Boolean> add(@RequestBody ShoppingCart shoppingCart) {
         return R.success(shoppingCartService.addUserShoppingCart(BaseContext.getCurrentUserId(), shoppingCart));
     }
 
+    @Operation(summary = "减少购物车项目")
     @PostMapping("/sub")
     public R<Boolean> sub(@RequestBody ShoppingCart shoppingCart) {
         return R.success(shoppingCartService.subUserShoppingCart(BaseContext.getCurrentUserId(), shoppingCart));
     }
 
+    @Operation(summary = "清空购物车项目")
     @DeleteMapping("/clean")
     public R<Boolean> clean() {
         return R.success(shoppingCartService.cleanUserShoppingCart(BaseContext.getCurrentUserId()));
