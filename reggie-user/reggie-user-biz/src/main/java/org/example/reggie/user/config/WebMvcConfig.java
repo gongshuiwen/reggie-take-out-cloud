f46b15170eda30ff9ce2949fc7b1b29cb09cc424
gongshuiwen <shuiwen.gong@outlook.com>
2024-01-06 23:07:47 +0800

    feat: add UserInfoInterceptor in reggie-user service

---
+++ reggie-user/reggie-user-biz/src/main/java/org/example/reggie/user/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package org.example.reggie.user.config;
+
+
+import org.example.reggie.common.interceptors.UserInfoInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration(proxyBeanMethods = false)
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserInfoInterceptor());
+    }
+}
